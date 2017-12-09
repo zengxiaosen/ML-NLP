aa928df8d192ba27912ecdf95e2c6fa1fac5a004
zengxiaosen <919451814@qq.com>
2017-12-09 23:46:54 +0800

    test

--- myproject/showproject/urls.py
+++ myproject/showproject/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url,include
 
 from . import views
 urlpatterns = [
+
     url(r'^index/', views.index),
     #url(r'^provice/', views.provice,name='provice'),
 
--- myproject/showproject/views.py
+++ myproject/showproject/views.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from django.http import HttpResponse
 import json
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html");
+    return render(request, 'index.html')
