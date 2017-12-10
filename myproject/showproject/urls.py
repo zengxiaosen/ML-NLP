e0d02ab357fb59bfd602820e9843ff35a0d7116a
zengxiaosen <919451814@qq.com>
2017-12-10 14:04:36 +0800

    test

--- myproject/showproject/urls.py
+++ myproject/showproject/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 
     url(r'^index/', views.index),
-    #url(r'^provice/', views.provice,name='provice'),
-
+    url(r'^provice/', views.provice, name='provice'),
+    url(r'^city/', views.city, name='city'),
+    url(r'^hello/', views.city, name='hello'),
 ]
--- myproject/showproject/views.py
+++ myproject/showproject/views.py
@@ -7,3 +7,26 @@ import json
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+def provice(request):
+    return render(request, 'provice.html')
+
+def city(request):
+    myjson = {
+        'type': 'column',
+        'colorByPoint': 'true',
+        'data': [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+        'showInLegend': 'false'
+    }
+    data = json.dumps(myjson)
+    return render(request, 'city.html', locals())
+
+def hello(request):
+    myjson = {
+        'type': 'column',
+        'colorByPoint': 'true',
+        'data': [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+        'showInLegend': 'false'
+    }
+    data = json.dumps(myjson)
+    return render(request, 'hello.html', locals())
+
